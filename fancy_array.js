68870fb943a6b83346131c96420373cec25fbcdf
kevinbundi <kevinjaunty@gmail.com>
2017-05-23 23:34:54 -0700

    js file modified

--- fancy_array.js
+++ fancy_array.js
@@ -1,11 +1,13 @@
 function printArray(symbol,bool){
 	var arr = ["James","Jill","Jane","Jack"]
 	if(bool==true){
+		//reverse array
 		for(var i=arr.length-1; i>=0; i-- ){
 			console.log(i+" " + symbol+" "+arr[i])
 		}
 	}
 	else{
+		//prints in order starting from the first element in the array 
 		for(var i=0; i<arr.length; i++){
 		console.log(i +  symbol + arr[i])
 		}
